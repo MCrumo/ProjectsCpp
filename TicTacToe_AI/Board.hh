c4d5b60e1009231791b49975f53695649b724037
MCrumo <miquel1rr@gmail.com>
2021-07-02 19:44:23 +0200

    random IA

--- TicTacToe_AI/Board.hh
+++ TicTacToe_AI/Board.hh
@@ -39,8 +39,8 @@ public:
     /*Post: returns true if the place is alowed, false otherwise.*/
   {
     if (place.size() != 2) return false;
-    short i = place[1] - 48;
-    short j = place[0] - 48;
+    short j = place[1] - 48;
+    short i = place[0] - 48;
     if (i >= 0 and i <= 2 and j >= 0 and j <= 2) {
       if (board[i][j] == ' ') return true;
       else return false;
@@ -48,31 +48,26 @@ public:
     else return false;
   }
   
-  bool end_game(bool& player1) 
+  bool end_game() 
     /*Pre: true*/
     /*Post: returns true if someone won and turns
     player1 to true if p1 is who won and flase otherwise*/
   {
-    player1 = false;
     for (int k = 0; k < 3; ++k) {
       if (board[k][0] == board[k][1] and board[k][1] == board[k][2]) {
-        if (board[k][0] == cp1) player1 = true;
-        return true;
+        if (board[k][0] != ' ') return true;
       }
     }
     for (int k = 0; k < 3; ++k) {
       if (board[0][k] == board[1][k] and board[1][k] == board[2][k]) {
-        if (board[0][k] == cp1) player1 = true;
-        return true;
+        if (board[0][k] != ' ') return true;
       }
     }
     if (board[0][0] == board[1][1] and board[1][1] == board[2][2]) {
-      if (board[1][1] == cp1) player1 = true;
-      return true;
+      if (board[1][1] != ' ') return true;
     }
     if (board[2][0] == board[1][1] and board[1][1] == board[0][2]) {
-      if (board[1][1] == cp1) player1 = true;
-      return true;
+      if (board[1][1] != ' ') return true;
     }
     return false;
   }
@@ -95,10 +90,29 @@ public:
     /*Post: adds and 'X' on place if player1 = true
     or an 'O' if player1 = false*/
   {
-    short i = place[1] - 48;
-    short j = place[0] - 48;
+    short j = place[1] - 48; //addr
+    short i = place[0] - 48;
     if (player1) board[i][j] = cp1;
     else board[i][j] = cp2;
+  }
+  
+  pair<int,int> random_play() 
+    /*Pre: the board is not full*/
+    /*Post: adds an 'O' to a random place on the board and returns a pair of int on witch position did it place it*/
+  {
+      bool cond = true;
+      pair<int, int> coord;
+      for (int i = 0; i < 3 and cond; ++i) {
+          for (int j = 0; j < 3 and cond; ++j) {
+              if (board[i][j] == ' ') {
+                  board[i][j] = 'O';
+                  cond = false;
+                  coord.first = i;
+                  coord.second = j;
+              }
+        }
+    }
+    return coord;
   }
   
   //InputOutput
--- TicTacToe_AI/main.cc
+++ TicTacToe_AI/main.cc
@@ -1,31 +1,32 @@
-#include "board.hh"
+#include "Board.hh"
 
 
 int main() {
-    bool ia = false;
-    bool cond = true;
-    string p1, p2, r1;
-    string q1 = "Will Player2 be an A.I. ?";
-    string q2 = "y/n";
-    
-    cout << q1 << endl << q2 << endl;
-    while (cond and cin >> r1) {
-        if (r1 == "y") {
-            ia = true;
-            cond = false;
+    bool playagain = true;
+    while (playagain) {
+        bool ia = false;
+        bool cond = true;
+        string p1, p2, r1;
+        string q1 = "Will Player2 be an A.I. ?";
+        string q2 = "y/n ";
+        
+        cout << endl << q1 << endl << q2;
+        while (cond and cin >> r1) {
+            if (r1 == "y") {
+                ia = true;
+                cond = false;
+            }
+            else if (r1 == "n") cond = false;
+            else cout << endl << q2;
         }
-        else if (r1 == "n") cond = false;
-        else cout << q2 << endl;
-    }
-    
-    Board board;
-    int cont = 0;
-    if (ia) cont = cont * 2; //useless
-    bool endgame = false;
-    while (not board.if_full() and not endgame) {
-        if (true /* not ia */) {
+        
+        Board board;
+        int cont = 0;
+        if (ia) cont = cont * 2; //useless
+        bool endgame = false;
+        board.print();
+        while (not board.if_full() and not endgame) {
             string place = "44";
-            bool p1win;
             if (cont%2 == 0) { //player1
                 while(not board.fear_move(place)) {
                     cout << endl << "P1: ";
@@ -33,24 +34,41 @@ int main() {
                 }
                 board.add_play(place, true);
                 board.print();
-                if (board.end_game(p1win)) {
+                if (board.end_game()) {
                     endgame = true;
                     cout << "Player 1 has won!" << endl;
                 }
             }
-            else { //player2
+            else if (not ia){ //player2
                 while(not board.fear_move(place)) {
                     cout << endl << "P2: ";
                     cin >> place;
                 }
                 board.add_play(place, false);
                 board.print();
-                if (board.end_game(p1win)) {
+                if (board.end_game()) {
                     endgame = true;
                     cout << "Player 2 has won!" << endl;
                 }
             }
+            else { //I.A. tourn
+                pair <int, int> coord = board.random_play();
+                cout << endl << "P2: " << coord.first << coord.second << endl;
+                board.print();
+                if (board.end_game()) {
+                    endgame = true;
+                    cout << "Player 2, aka the I.A. has won!" << endl;
+                }
+            }
+            ++cont;
         }
-        ++cont;
-    }
+        string ans = "answ";
+        cout << "Play again?" << endl << q2;
+        while (ans != "n" and ans != "y") {
+            cin >> ans;
+            cout << " ";
+        }
+        if (ans == "n") playagain = false;
+        
+    } 
 }
